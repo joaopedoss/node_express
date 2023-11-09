53a68fc78e582b70f01d2c2ef9cb211cd84b8e5a
Joao Pedro <jpss06joao@gmail.com>
2023-11-09 07:59:29 -0300

    ultimo commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express")
+const path = require("path")
 
-application.get('/', (requisica, resposta) => {
-    resposta.send("Estou funcionando com o express")
+const app = express()
+
+const caminhoBase= path.join(__dirname, "templates")
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send(`${caminhoBase}/index.html`)
+    console.log(caminhoBase)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
 })
