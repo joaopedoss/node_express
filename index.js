2364858bc524acb2018127b0bc7b3018b2169728
Joao Pedro <jpss06joao@gmail.com>
2023-11-10 09:49:57 -0300

    usuarios

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express()
 
 const caminhoBase= path.join(__dirname, "templates")
 
+
+app.get('/usuarios', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
